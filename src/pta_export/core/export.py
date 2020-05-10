7afd39576df89bb229d5a063df853977c53fd6fe
Sergei Maertens <sergei@maykinmedia.nl>
2020-05-10 18:22:20 +0200

    :lipstick: Style table a bit

--- src/pta_export/core/export.py
+++ src/pta_export/core/export.py
@@ -8,6 +8,11 @@ from django.utils.text import capfirst
 
 from docx import Document
 from docx.enum.section import WD_ORIENT
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT, WD_ROW_HEIGHT_RULE
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.oxml import parse_xml
+from docx.oxml.ns import nsdecls
+from docx.shared import Inches, Mm, Pt
 
 from .constants import Leerjaren
 from .models import Kalender, Toets, Vak
@@ -22,6 +27,20 @@ LEERJAAR_WEGING = {
     Leerjaren.vwo_5: ("Weging ED5", "weging_ed5"),
     Leerjaren.vwo_6: ("Weging ED6", "weging_ed6"),
 }
+
+
+COLUMN_WIDTHS = {
+    0: Inches(0.597),
+    1: Inches(4.106),
+    2: Inches(1.279),
+    3: Inches(0.69),
+    4: Inches(0.59),
+    5: Inches(0.886),
+    6: Inches(0.59),
+    "default": Inches(0.689),
+}
+
+HEADER_BG_COLOR = "D9D9D9"
 
 
 def export(year: int, leerjaar: int):
@@ -44,52 +63,6 @@ def export(year: int, leerjaar: int):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as tmp:
         doc.save(tmp.name)
         print(f"Output in: {tmp.name}")
-
-
-def create_document(year: int, leerjaar: int, vakken: Iterable[Vak],) -> Document:
-    toetsweken = Kalender.objects.get(jaar=year).toetsweken
-    weging = LEERJAAR_WEGING.get(leerjaar)
-    _leerjaar = Leerjaren.labels[leerjaar]
-    school_year = f"{year}-{year + 1}"
-
-    document = Document()
-
-    # set to landscape
-    section = document.sections[-1]
-    new_width, new_height = section.page_height, section.page_width
-    section.orientation = WD_ORIENT.LANDSCAPE
-    section.page_width = new_width
-    section.page_height = new_height
-
-    # add the document title
-    document.add_heading(f"PTAs {_leerjaar} {year}", level=0)
-
-    for vak in vakken:
-        header = f"PTA\t{capfirst(vak.naam)}\t{_leerjaar}\t{school_year}"
-        document.add_heading("", level=1).add_run(header).bold = True
-
-        [header, *rows] = get_toets_table(vak, toetsweken, weging)
-
-        table = document.add_table(rows=len(rows) + 1, cols=len(header))
-
-        header_cells = table.rows[0].cells
-        for index, title in enumerate(header):
-            header_cells[index].text = title
-            _make_bold(header_cells[index])
-
-        for index, row in enumerate(rows, 1):
-            row_cells = table.rows[index].cells
-            for _index, content in enumerate(row):
-                row_cells[_index].text = str(content)
-            _make_bold(row_cells[0])
-
-        document.add_page_break()
-
-    return document
-
-
-def _make_bold(cell):
-    cell.paragraphs[0].runs[0].font.bold = True
 
 
 def get_toets_table(
@@ -123,7 +96,7 @@ def get_toets_table(
             periode,
             toets.week or "",
             toets.soortwerk.naam,
-            toets.tijd,
+            toets.tijd or "",
             toets.weging_r4 or "",
         ]
 
@@ -134,3 +107,83 @@ def get_toets_table(
         rows.append(row)
 
     return [header] + rows
+
+
+def create_document(year: int, leerjaar: int, vakken: Iterable[Vak],) -> Document:
+    toetsweken = Kalender.objects.get(jaar=year).toetsweken
+    weging = LEERJAAR_WEGING.get(leerjaar)
+    _leerjaar = Leerjaren.labels[leerjaar]
+    school_year = f"{year}-{year + 1}"
+
+    document = Document()
+
+    # set to landscape
+    section = document.sections[-1]
+    section.orientation = WD_ORIENT.LANDSCAPE
+    # A4 page size
+    section.page_width = Mm(297)
+    section.page_height = Mm(210)
+
+    # add the document title
+    document.add_heading(f"PTAs {_leerjaar} {year}", level=0)
+
+    for vak in vakken:
+        header = f"PTA\t{capfirst(vak.naam)}\t{_leerjaar}\t{school_year}"
+        document.add_heading("", level=1).add_run(header).bold = True
+
+        [header, *rows] = get_toets_table(vak, toetsweken, weging)
+
+        table = document.add_table(rows=len(rows) + 1, cols=len(header))
+        table.style = "TableGrid"
+        table.autofit = False
+
+        header_cells = table.rows[0].cells
+        for index, title in enumerate(header):
+            cell = header_cells[index]
+            cell.text = title
+            _make_bold(cell)
+            _set_cell_bg(cell)
+
+        for index, row in enumerate(rows, 1):
+            row_cells = table.rows[index].cells
+            for _index, content in enumerate(row):
+                row_cells[_index].text = str(content)
+
+        # style the cells: widths, font
+        for row in table.rows:
+            _make_bold(row.cells[0])
+            for index, cell in enumerate(row.cells):
+                center = index != 1
+                _style_cell(cell, center=center)
+
+        for index, column in enumerate(table.columns):
+            column.width = COLUMN_WIDTHS.get(index, COLUMN_WIDTHS["default"])
+
+        for row in table.rows:
+            row.height_rule = WD_ROW_HEIGHT_RULE.AT_LEAST
+            row.height = Inches(0.276)
+
+        document.add_page_break()
+
+    return document
+
+
+def _make_bold(cell):
+    cell.paragraphs[0].runs[0].font.bold = True
+
+
+def _style_cell(cell, center=True):
+    cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER
+
+    for paragraph in cell.paragraphs:
+        if center:
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        for run in paragraph.runs:
+            run.font.name = "Arial"
+            run.font.size = Pt(10)
+
+
+def _set_cell_bg(cell, color: str = HEADER_BG_COLOR):
+    shading_elm_1 = parse_xml(r'<w:shd {} w:fill="{}"/>'.format(nsdecls("w"), color))
+    cell._tc.get_or_add_tcPr().append(shading_elm_1)
